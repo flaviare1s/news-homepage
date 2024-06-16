3d09df06dfe32c0d8f18e725b98f507311ef8d3a
flaviare1s <flaviarster@gmail.com>
2024-06-16 11:33:36 -0300

    Working on burger menu

---
+++ script.js
@@ -0,0 +1,8 @@
+const menuIcon = document.querySelector('#burger-menu');
+const closeIcon = document.querySelector('#close-menu');
+const nav = document.querySelector('#nav');
+const overlay = document.querySelector('#menu-overlay');
+
+menuIcon.addEventListener('click', () => {
+  
+})

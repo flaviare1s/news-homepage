2a5929c550eb9f4d32360c8cf70b5b997a8a84b5
flaviare1s <flaviarster@gmail.com>
2024-06-16 11:59:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,11 +3,24 @@ const closeIcon = document.querySelector('#close-menu');
 const nav = document.querySelector('#nav');
 const overlay = document.querySelector('#menu-overlay');
 
-menuIcon.addEventListener('click', () => {
+function openMenu() {
   overlay.classList.remove('hidden')
   overlay.classList.add('active')
   nav.classList.add('active')
   menuIcon.classList.add('hidden')
   closeIcon.classList.remove('hidden')
   closeIcon.classList.add('active')
-})
+}
+
+function closeMenu() {
+  overlay.classList.remove('active')
+  overlay.classList.add('hidden')
+  nav.classList.remove('active')
+  menuIcon.classList.remove('hidden')
+  menuIcon.classList.add('active')
+  closeIcon.classList.remove('active')
+  closeIcon.classList.add('hidden')
+}
+
+menuIcon.addEventListener('click', openMenu)
+closeIcon.addEventListener('click', closeMenu)
